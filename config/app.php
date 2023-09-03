a0150e1c535404859d8dc5847aed76b71073abd6
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-03 16:00:06 +0200

    uff ...

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-        App\Providers\NovaServiceProvider::class,
+        //App\Providers\NovaServiceProvider::class,
         App\Providers\MasteringNovaProvider::class,
         /*
          * Application Service Providers...
@@ -174,7 +174,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
+        //App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
